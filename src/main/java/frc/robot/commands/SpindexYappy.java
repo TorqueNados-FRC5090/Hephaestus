747b8052d23be33767535048621fdf39dea9e80c
eden :D <3thereal3den@gmail.com>
2026-05-26 19:38:50 -0400

    spindex leader follower

---
+++ src/main/java/frc/robot/commands/SpindexYappy.java
@@ -0,0 +1,45 @@
+ 
+package frc.robot.commands;
+import java.util.function.BooleanSupplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.subsystems.Spindex;
+
+public class SpindexYappy extends Command{
+    Spindex spinner;
+    BooleanSupplier runCondition;
+
+    public SpindexYappy(Spindex spinner, BooleanSupplier runCondition){
+        this.spinner = spinner;
+        this.runCondition = runCondition;
+
+        addRequirements(spinner);
+    }
+    
+    @Override
+    public void initialize(){
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        if (runCondition.getAsBoolean())
+            spinner.spin(-30, -.9);
+        else spinner.spindexStop();
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+       spinner.spindexStop();
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false; // Has no end condition
+    }
+    
+}
+
